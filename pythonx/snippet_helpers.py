f35e411066542786ad674905b993707abfcb01c4
Lajos Koszti <koszti.lajos@ustream.tv>
2017-01-18 20:55:23 +0100

    Fix python snippet names add php

--- pythonx/snippet_helpers.py
+++ pythonx/snippet_helpers.py
@@ -13,3 +13,9 @@ def getModuleNameFromPath(snip, path):
         output = os.path.basename(os.path.dirname(os.path.abspath(path)))
 
     snip.rv = output
+
+
+def getModuleNameFromFilename(snip):
+    splitted_name = re.sub(r'([^a-z]+)', r'_\1', snip.basename).split('_')
+    filtered_empty = filter(lambda f: f != '', splitted_name)
+    snip.rv = ''.join(map(lambda x: x.title(), filtered_empty))
