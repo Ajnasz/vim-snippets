d8628e8b2c1314d4fccf0a2eebe00b67c8829483
Lajos Koszti <ajnasz@ajnasz.hu>
2017-11-07 09:54:15 +0100

    Ignore git compiled files

--- pythonx/snippet_helpers.py
+++ pythonx/snippet_helpers.py
@@ -1,11 +1,9 @@
 import os
 import re
 
-
 def getTitleFromSnip(snip):
     splitted_name = re.sub(r'([^a-z]+)', r'_\1', snip.basename).split('_')
     snip.rv = ' '.join(filter(lambda f: f != '', splitted_name)).title()
-
 
 def getModuleNameFromPath(snip, path):
     output = 'ModuleName'
@@ -13,7 +11,6 @@ def getModuleNameFromPath(snip, path):
         output = os.path.basename(os.path.dirname(os.path.abspath(path)))
 
     snip.rv = output
-
 
 def getModuleNameFromFilename(snip):
     splitted_name = re.sub(r'([^a-z]+)', r'_\1', snip.basename).split('_')
